e5d87bca59cb03e47c6ddd8f9114d8eb283c1938
joygram <joygram@gmail.com>
2025-03-31 12:32:21 +0900

    use kanata gui cmd allowed

--- start_kanata.cmd
+++ start_kanata.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd .
 cd %~dp0
-powershell "start kanata_gui_wintercept_cmd_allowed.exe -Args \"--cfg kanata.kbd\" -WindowStyle Hidden"
+powershell "start kanata_gui_cmd_allowed.exe -Args \"--cfg kanata.kbd\" -WindowStyle Hidden"
 popd 
