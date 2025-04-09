27b792953de93f812421430bc4b3c423bd329d05
joygram <joygram@gmail.com>
2025-04-09 12:39:06 +0900

    add brace

--- start_kanata.cmd
+++ start_kanata.cmd
@@ -2,5 +2,6 @@
 pushd .
 cd %~dp0
 rem start powershell "start kanata_gui_cmd_allowed.exe -Args \"--cfg kanata.kbd\" -WindowStyle Hidden"
+taskkill /f /im kanata*
 start kanata_gui_cmd_allowed.exe --cfg kanata.kbd
-popd 
+popd
